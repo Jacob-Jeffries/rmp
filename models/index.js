8a7bbe571215b956f7865b902d17476e7ef260a8
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-07 22:07:33 -0500

    Started the index.js model linakges for the models

--- models/index.js
+++ models/index.js
@@ -1,1 +1,14 @@
 // Joins and such
+const User = require('./user');
+const Pet = require('./pet');
+const Rating = require('./ratings');
+
+Gallery.hasMany(Painting, {
+  foreignKey: 'gallery_id',
+});
+
+Painting.belongsTo(Gallery, {
+  foreignKey: 'gallery_id',
+});
+
+module.exports = { User, Gallery, Painting };
