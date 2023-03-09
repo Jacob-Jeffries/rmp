5a1d2f0a2786f15275b67b42945efb043ec1b189
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-09 17:25:59 -0500

    Created the PET and RATING models, modified the PET seed data to include user_id as a foreign key

--- models/pet.js
+++ models/pet.js
@@ -1,1 +1,58 @@
 // Pet Model
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Pet extends Model {}
+
+Pet.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    pet_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.ENUM('Male', 'Female'),
+      allowNull: false,
+    },
+    special_skills: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    favorite_toy: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    filename: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+        unique: false
+      }
+    },
+    owner_rating: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'pet',
+  }
+);
+
+module.exports = Pet;
