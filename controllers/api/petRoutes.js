2bf99677f9231e6b1cb9b20e77940ba4e1df4e73
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-09 20:00:16 -0500

    started pet routes

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -1,1 +1,11 @@
 // Modular GET & POST routes for Pets
+
+const pet = require('express').Router();
+const { Pet } = require('../../models');
+
+pet.get('/', async (req, res) => {
+  try {
+    const 
+  } catch (err) {
+  }
+});
