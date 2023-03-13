f89e8df437040468b473ec406a6144380196ef48
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-03-13 19:19:49 -0400

    nav link routes completed

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@
 
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
