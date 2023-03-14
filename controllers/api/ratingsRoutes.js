7bcfba5b363ceb3f549a2b92f7f6963c58c87ded
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-13 22:07:14 -0400

    Created routes in ratingsRoutes.js: Create (POST), Get all (GET), Get all ratings by pet_id (GET), Get averge ratings for pet_id (GET)

--- controllers/api/ratingsRoutes.js
+++ controllers/api/ratingsRoutes.js
@@ -1,1 +1,62 @@
 // GET & POST for the individual pet ratings
+const router = require('express').Router();
+const { Rating } = require('../../models');
+
+// Create a new rating
+router.post('/', async (req, res) => {
+  try{
+    const ratingData = await Rating.create(req.body);
+    res.status(200).json(ratingData);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+// Get all ratings
+router.get('/', async (req, res) => {
+  try{
+    const ratingData = await Rating.findAll()
+    res.status(200).json(ratingData);
+  }catch(err){
+      res.status(500).json(err);
+    }
+});
+
+// Get all ratings for a specific pet_id
+router.get('/:pet_id', async (req, res) => {
+  try{
+    const ratingData = await Rating.findAll({
+      where: {
+        pet_id: req.params.pet_id
+      }
+    })
+    res.status(200).json(ratingData);
+  }catch(err){
+      res.status(500).json(err);
+    }
+});
+
+// Get average ratings for pet_id
+router.get('/:pet_id/avg', async (req, res) => {
+  try{
+    const count = await Rating.count({
+      where: {
+        pet_id: req.params.pet_id
+    }}); 
+    
+    const sumRating = await Rating.sum('rank', {
+      where: {
+        pet_id: req.params.pet_id
+    }});
+
+    const pet_id = parseInt(req.params.pet_id);
+    
+    const average = { "pet_id": pet_id, "average_rating" : sumRating / count, "total_rating" : sumRating, "rating_count" : count};
+
+    res.status(200).json(average);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
