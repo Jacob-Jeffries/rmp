f330dbc71960db2cbd4b2aaff6f7c5ced158ded8
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-14 21:32:43 -0400

    added multer route, but it is giving me an error right now

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -2,15 +2,19 @@
 
 const router = require('express').Router();
 const { Pet } = require('../../models');
-const { update } = require('../../models/user');
+const multer = require('multer');
+const upload = multer({ dest: 'images/' });
 
-router.post('/', async (req, res) => {
-  try {
-    const petData = await Pet.create(req.body);
-    res.status(200).json(petData);
-  }catch(err){
-    res.status(500).json(err);
-  }
+
+router.post('/', upload.single('upload_image'), async (req, res) => {
+  console.log(req.file, req.body);
+  // try {
+  //   console.log(req.body);
+  //   const petData = await Pet.create(req.body);
+  //   res.status(200).json(petData);
+  // }catch(err){
+  //   res.status(500).json(err);
+  // }
 });
 
 router.get('/', async (req, res) => {
