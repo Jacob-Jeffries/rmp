8d7e368c379e5904eb0a49334d3c0964d2cf314e
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-19 15:23:03 -0400

    changed userRoutes.jd back; updated login.js line 8 & 15 to point to heroku

--- public/js/login.js
+++ public/js/login.js
@@ -5,14 +5,14 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    const response = await fetch('/api/users/login', {
+    const response = await fetch('https://murmuring-garden-13240.herokuapp.com/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('https://murmuring-garden-13240.herokuapp.com/');
     } else {
       alert('Failed to log in.');
     }
